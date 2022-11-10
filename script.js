4001edf5141a93887cd27aa6858a781c9041a635
João Pedro T <joaopedrot1912@gmail.com>
2022-11-10 20:25:30 -0300

    update 10/11

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ let tentativas_restantes = document.querySelector("p#tentRest")
 let resultado = document.getElementById("resposta")
 let Nescolhidos = []
 let detector_rep = 0
-const numero_sorteado = sortear()
-let detecPerdedor = 0
+let numero_sorteado = sortear()
+let detecPerdedor = 1
+let tent = 5
+let valorRecebido = document.getElementById("valor")
+let resp_numeroSort = document.getElementById("numero_sort")
 
-  
+
 function sortear(){
     let Nsorteado = Math.ceil(Math.random() * 50)
     return Nsorteado
@@ -18,7 +21,6 @@ function Verificar(){
     if (valor < 1 || valor > 50) {
         alert("[ERRO]: valor ínvalido!")
     } else {
-        valorRecebido.innerHTML = ""
         if (Nescolhidos.length != 0) {
             for (let c = 0 ; c < 4 ; c++) {
                 if (valor == Nescolhidos[c]) {
@@ -27,10 +29,16 @@ function Verificar(){
                 } 
             }
         }
+
+        valorRecebido.value = ''
+        valorRecebido.focus()
         if (detector_rep == 0) {
+            tent -= 1
             Nescolhidos.push(valor)
+            tentativas_restantes.innerHTML = `<ins>Tentativas restantes: ${tent}</ins>`
         }
     }
+
   if (Nescolhidos.length == 5) {
         resul()    
   }
@@ -40,16 +48,21 @@ function Verificar(){
 function resul() {
     for(let c = 0 ; c < 5 ; c++) {
         if (Nescolhidos[c] == numero_sorteado) {
-            detecPerdedor = 1
             vencedor()
-            
-        }
+        } 
     }
     if (detecPerdedor == 1) {
-        resultado.innerHTML = "[RESULTADO]: Lamendo, você não digitou o número vencedor."
+        perdedor()
     }
 }
 
 function vencedor() {
+    detecPerdedor = 0
     resultado.innerHTML = "[RESULTADO]: Você acertou o número vencedor, PARABENS!!!"
+    resp_numeroSort.innerHTML = `Número ganhador: ${numero_sorteado}`
+}
+
+function perdedor() {
+    resultado.innerHTML = "[RESULTADO]: Lamendo, você não digitou o número vencedor."
+    resp_numeroSort.innerHTML = `<p><strong>Número ganhador: ${numero_sorteado}</strong></p>`
 }
