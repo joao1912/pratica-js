f43ed7fdc3779802bad3c3b34e3e93b19acd480f
João Pedro T <joaopedrot1912@gmail.com>
2022-11-10 13:48:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 let tentativas_restantes = document.querySelector("p#tentRest")
-//let valor = document.getElementById("valor") //erro NULL
 let resultado = document.getElementById("resposta")
 let Nescolhidos = []
 let detector_rep = 0
+const numero_sorteado = sortear()
+let detecPerdedor = 0
 
-
+  
 function sortear(){
-    const Nsorteado = Math.ceil(Math.random() * 50)
+    let Nsorteado = Math.ceil(Math.random() * 50)
     return Nsorteado
 }
 
-
 function Verificar(){
-    
+    let valorRecebido = document.getElementById("valor")
+    let valor = valorRecebido.value
     detector_rep = 0
-   
     if (valor < 1 || valor > 50) {
         alert("[ERRO]: valor ínvalido!")
     } else {
@@ -26,9 +26,29 @@ function Verificar(){
                 } 
             }
         }
-        if (detector_rep = 0) {
+        if (detector_rep == 0) {
             Nescolhidos.push(valor)
         }
     }
+  if (Nescolhidos.length == 5) {
+        resul()    
+  }
   
+}
+
+function resul() {
+    for(let c = 0 ; c < 5 ; c++) {
+        if (Nescolhidos[c] == numero_sorteado) {
+            detecPerdedor = 1
+            vencedor()
+            
+        }
+    }
+    if (detecPerdedor == 1) {
+        resultado.innerHTML = "[RESULTADO]: Lamendo, você não digitou o número vencedor."
+    }
+}
+
+function vencedor() {
+    resultado.innerHTML = "[RESULTADO]: Você acertou o número vencedor, PARABENS!!!"
 }
